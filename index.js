8d24802c2d1d6700374e52f63ba782fc63fd0c29
Nick Kotok <lennakz@gmail.com>
2021-04-12 00:55:47 -0700

    First pull request

--- index.js
+++ index.js
@@ -3,19 +3,23 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+	// Your code here
+	app.log.info('Yay, the app was loaded!')
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+	// app.on("issues.opened", async (context) => {
+	//   const issueComment = context.issue({
+	//     body: "Thanks for opening this issue!",
+	//   });
+	//   return context.octokit.issues.createComment(issueComment);
+	// });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+	app.on('pull_request.opened', async (context) => {
+    console.log('pull request opened')
+  })
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-};
+	// For more information on building apps:
+	// https://probot.github.io/docs/
+
+	// To get your app running against GitHub, see:
+	// https://probot.github.io/docs/development/
+}
